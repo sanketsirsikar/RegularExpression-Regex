7b11321b714534e73b876f379cf6862db255f93e
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-11-27 17:20:31 +0530

    uc1 valid firstName added

--- src/com/bridgelabz/CheckValidation.java
+++ src/com/bridgelabz/CheckValidation.java
@@ -1,2 +1,17 @@
-package com.bridgelabz;public class CheckValidation {
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class CheckValidation {
+    void checkFirstName(){
+        System.out.println("Enter First Name");
+        Scanner sc=new Scanner(System.in);
+        String firstName=sc.nextLine();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{3,}$",firstName)){
+            System.out.println("First Name is Valid");
+        }else
+            System.out.println("First Name is Invalid");
+
+    }
 }
